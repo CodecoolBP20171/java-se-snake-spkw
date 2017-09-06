2eef57b54435c3cd2351910480fc275597879c03
norbertHauriel <hauriel.norbert@gmail.com>
2017-09-06 23:42:36 +0200

    Bouncing from margin
    - Created margin constants (right, bottom) used for generating correct enemy spawn.
    - Enemies bounce from margins by turning 180 degrees.
    - Overloaded isOutOfBounds() to take margin inputs.

--- src/com/codecool/snake/entities/enemies/advancedEnemy1.java
+++ src/com/codecool/snake/entities/enemies/advancedEnemy1.java
@@ -1,9 +1,9 @@
 package com.codecool.snake.entities.enemies;
 
-import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
-import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.Utils;
+import com.codecool.snake.entities.Animatable;
+import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.geometry.Point2D;
@@ -13,11 +13,12 @@ import java.security.SecureRandom;
 
 public class advancedEnemy1 extends GameEntity implements Animatable, Interactable {
 
-    private Point2D heading;
     private static final int damage = 20;
-    private double direction;
+    private final int RIGHT_MARGIN = 30, BOTTOM_MARGIN = 30;
     int speed;
     SecureRandom rnd;
+    private Point2D heading;
+    private double direction;
 
     public advancedEnemy1(Pane pane) {
         super(pane);
@@ -25,8 +26,8 @@ public class advancedEnemy1 extends GameEntity implements Animatable, Interactab
         this.rnd = new SecureRandom();
         setImage(Globals.simpleEnemy);
         pane.getChildren().add(this);
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+        setX(rnd.nextDouble() * (Globals.WINDOW_WIDTH - RIGHT_MARGIN));
+        setY(rnd.nextDouble() * (Globals.WINDOW_HEIGHT - BOTTOM_MARGIN));
         this.direction = rnd.nextDouble() * 360;
         setRotate(direction);
         this.heading = Utils.directionToVector(direction, speed);
@@ -35,8 +36,15 @@ public class advancedEnemy1 extends GameEntity implements Animatable, Interactab
 
     @Override
     public void step() {
+
+        // Check for collision with margin
+        if (isOutOfBounds(0, RIGHT_MARGIN, 0, BOTTOM_MARGIN)) {
+            direction += 180;
+            heading = Utils.directionToVector(direction, speed);
+        }
+
         // Random chance to change direction
-        if (rnd.nextInt(100) == 0) {
+        else if (rnd.nextInt(100) == 0) {
             direction = Utils.randomizeDirection(direction, 60);
             heading = Utils.directionToVector(direction, speed);
         }
@@ -53,6 +61,5 @@ public class advancedEnemy1 extends GameEntity implements Animatable, Interactab
     @Override
     public String getMessage() {
         return damage + " damage from " + advancedEnemy1.class.getSimpleName();
-
     }
 }
